dfbde98c3c4a315ade40638583821089e53975a8
bomber228qw <iaroslavfedotov@icloud.com>
2022-11-26 17:15:10 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Main {
     static Scanner console = new Scanner(System.in);
     public static void main(String[] args) {
-        int a = console.nextInt();
+        System.out.print("Введите число: ");
+        int z = console.nextInt();
 
-        System.out.println(a >=12 && a<= 33);
+        if (z<10000){
+            System.out.println(  z + " не превышает лимит, проходите");
+        } else if (z > 10000) {
+            System.out.println("Ого! " +z+ "!Куда вам столько? Мы заберем " +z+1000);
+        }
     }
-    }
+}
--- src/less0n7.java
+++ src/less0n7.java
@@ -4,17 +4,15 @@ public class less0n7 {
     static Scanner consolt = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int z =1;
-        int count =0;
-        int conut =0;
-        while (z != 0) {
-            System.out.print("Введите число: ");
-            z = consolt.nextInt();
-            if (z != 0) {
-                count++;
-                conut+=z;
-            }
+        int z = 1;
+        System.out.print("Введите число: ");
+        z = consolt.nextInt();
+
+        //int count =0;
+        int conut = 0;
+        while (conut * conut <= z) {
+            System.out.println(conut * conut);
+            conut++;
         }
-        System.out.println(conut/count);
     }
 }
